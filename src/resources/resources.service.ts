243f6d2399d2474b81f1ecb4b6a3a6c882348c34
Jaume Barcelo <jaume.barcelo@gmail.com>
2026-05-20 17:57:55 +0200

    changes resource services to use repository

--- src/resources/resources.service.ts
+++ src/resources/resources.service.ts
@@ -3,35 +3,34 @@ import type { CreateResourceDto } from './dto/create-resource.dto';
 import type { FindResourcesQueryDto } from './dto/find-resources-query.dto';
 import type { UpdateResourceDto } from './dto/update-resource.dto';
 import { Resource } from './resource.model';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ResourcesService {
-  private resources: Resource[] = [
-    {
-      id: 1,
-      name: 'Test Resource',
-      type: 'laptop',
-      status: 'available',
-      location: 'cyberspace',
-      createdAt: new Date().toISOString(),
-    },
-  ];
+  constructor(
+    @InjectRepository(Resource)
+    private readonly resourcesRepository: Repository<Resource>,
+  ) {}
 
-  findAll(query: FindResourcesQueryDto): Resource[] {
+  findAll(query: FindResourcesQueryDto): Promise<Resource[]> {
     const { type, status } = query;
 
-    return this.resources.filter((resource) => {
-      const matchesType = type === undefined || resource.type === type;
-      const matchesStatus = status === undefined || resource.status === status;
+    const where: Partial<Resource> = {};
 
-      return matchesStatus && matchesType;
-    });
+    if (type != undefined) {
+      where.type = type;
+    }
+
+    if (status != undefined) {
+      where.status = status;
+    }
+
+    return this.resourcesRepository.find({ where });
   }
 
-  findOne(id: number): Resource {
-    const resource = this.resources.find(
-      (currentResource) => currentResource.id === id,
-    );
+  async findOne(id: number): Promise<Resource> {
+    const resource = await this.resourcesRepository.findOneBy({ id });
 
     if (!resource) {
       throw new NotFoundException(`Resource with id ${id} not found`);
@@ -40,60 +39,43 @@ export class ResourcesService {
     return resource;
   }
 
-  create(createResourceDto: CreateResourceDto): Resource {
-    const newResource: Resource = {
-      id: this.resources.length + 1,
+  create(createResourceDto: CreateResourceDto): Promise<Resource> {
+    const newResource = this.resourcesRepository.create({
       name: createResourceDto.name,
       type: createResourceDto.type,
       status: 'available',
       location: createResourceDto.location,
       createdAt: new Date().toISOString(),
-    };
+    });
 
-    this.resources.push(newResource);
-
-    return newResource;
+    return this.resourcesRepository.save(newResource);
   }
 
-  update(id: number, updateResourceDto: UpdateResourceDto): Resource {
-    const resourceIndex = this.resources.findIndex(
-      (resource) => resource.id === id,
-    );
+  async update(
+    id: number,
+    updateResourceDto: UpdateResourceDto,
+  ): Promise<Resource> {
+    const resource = await this.findOne(id);
 
-    if (resourceIndex === -1) {
-      throw new NotFoundException(`Resource with id ${id} not found`);
-    }
-
-    const currentResource = this.resources[resourceIndex];
     const {
-      name = currentResource.name,
-      type = currentResource.type,
-      status = currentResource.status,
-      location = currentResource.status,
+      name = resource.name,
+      type = resource.type,
+      status = resource.status,
+      location = resource.status,
     } = updateResourceDto;
 
-    this.resources[resourceIndex] = {
-      ...currentResource,
+    return this.resourcesRepository.save({
+      ...resource,
       name,
       type,
       status,
       location,
-    };
-
-    return this.resources[resourceIndex];
+    });
   }
 
-  remove(id: number): Resource {
-    const resourceIndex = this.resources.findIndex(
-      (resource) => resource.id === id,
-    );
+  async remove(id: number): Promise<Resource> {
+    const resource = await this.findOne(id);
 
-    if (resourceIndex === -1) {
-      throw new NotFoundException(`Resource with id ${id} not found`);
-    }
-
-    const [deletedResource] = this.resources.splice(resourceIndex, 1);
-
-    return deletedResource;
+    return this.resourcesRepository.remove(resource);
   }
 }
