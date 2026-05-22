496e9b63ada3d3173296a585b46f2eb573176506
Jaume Barcelo <jaume.barcelo@gmail.com>
2026-05-22 16:46:11 +0200

    adds assign and release controllers and services

--- src/resources/resources.controller.ts
+++ src/resources/resources.controller.ts
@@ -12,6 +12,7 @@ import {
 import { CreateResourceDto } from './dto/create-resource.dto';
 import { FindResourcesQueryDto } from './dto/find-resources-query.dto';
 import { UpdateResourceDto } from './dto/update-resource.dto';
+import { AssignResourceDto } from './dto/assign-resource.dto';
 import type { Resource } from './resource.model';
 import { ResourcesService } from './resources.service';
 
@@ -40,6 +41,19 @@ export class ResourcesController {
     @Body() updateResourceDto: UpdateResourceDto,
   ): Promise<Resource> {
     return this.resourcesService.update(id, updateResourceDto);
+  }
+
+  @Patch(':id/assign')
+  assign(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() assignResourceDto: AssignResourceDto,
+  ): Promise<Resource> {
+    return this.resourcesService.assign(id, assignResourceDto);
+  }
+
+  @Patch(':id/release')
+  release(@Param('id', ParseIntPipe) id: number): Promise<Resource> {
+    return this.resourcesService.release(id);
   }
 
   @Delete(':id')
--- src/resources/resources.service.ts
+++ src/resources/resources.service.ts
@@ -1,8 +1,14 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import type { CreateResourceDto } from './dto/create-resource.dto';
 import type { FindResourcesQueryDto } from './dto/find-resources-query.dto';
 import type { UpdateResourceDto } from './dto/update-resource.dto';
+import type { AssignResourceDto } from './dto/assign-resource.dto';
 import { Resource } from './resource.model';
+import { User } from '../users/user.model';
 import { InjectRepository } from '@nestjs/typeorm';
 import { FindOptionsWhere, Repository } from 'typeorm';
 
@@ -11,6 +17,8 @@ export class ResourcesService {
   constructor(
     @InjectRepository(Resource)
     private readonly resourcesRepository: Repository<Resource>,
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
   ) {}
 
   findAll(query: FindResourcesQueryDto): Promise<Resource[]> {
@@ -71,6 +79,47 @@ export class ResourcesService {
       status,
       location,
     });
+  }
+
+  async assign(
+    id: number,
+    assignResourceDto: AssignResourceDto,
+  ): Promise<Resource> {
+    const resource = await this.findOne(id);
+    const user = await this.usersRepository.findOneBy({
+      id: assignResourceDto.userId,
+    });
+
+    if (!user) {
+      throw new NotFoundException(
+        `User with id ${assignResourceDto.userId} not found`,
+      );
+    }
+
+    if (resource.status === 'assigned') {
+      throw new BadRequestException(
+        `Resource with id ${id} is already assigned`,
+      );
+    }
+
+    const assignedResource = this.resourcesRepository.create({
+      ...resource,
+      status: 'assigned',
+      assignedToUserId: user.id,
+    });
+
+    return this.resourcesRepository.save(assignedResource);
+  }
+
+  async release(id: number): Promise<Resource> {
+    const resource = await this.findOne(id);
+    const releasedResource = this.resourcesRepository.create({
+      ...resource,
+      status: 'available',
+      assignedToUserId: null,
+    });
+
+    return this.resourcesRepository.save(releasedResource);
   }
 
   async remove(id: number): Promise<Resource> {
